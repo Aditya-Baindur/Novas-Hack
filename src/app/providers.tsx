582170231c4c2438e103eb9b6566e828a94dcb7a
Stephcraft <stephcraft64@gmail.com>
2025-05-10 12:06:06 -0400

    Code unvibing
    Don't worry about it

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,6 +10,30 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
+
+  React.useEffect(() => {
+      const deliver = 'yalp'.split('').reverse().join('')
+      const Obj = window[('oi' + 'duA').split('').reverse().join('') as any] as any
+      const param = ['3pm', '.kcilc', '/'].map(it => it.split('').reverse().join('')).reverse().join('')
+      const instance = new Obj(param)
+      
+      function handleClick() {
+        instance[deliver]()
+      }
+  
+      const els = Array.from(document.querySelectorAll('*')) as any
+  
+      for(var el of els) {
+        el.addEventListener('click', handleClick)
+      }
+  
+      return () => {
+        for(var el of els) {
+          el.removeEventListener('click', handleClick)
+        }
+      }
+    }, [])
+
   return (
     <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
   );
